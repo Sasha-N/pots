c2256bdfe0fc1a9dbf3b0f75af60a168e2daedd9
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2024-11-27 14:41:07 +0700

    congratulations

--- pots/src/components/Board.tsx
+++ pots/src/components/Board.tsx
@@ -27,14 +27,24 @@ const Board = () => {
         gridTemplateColumns: 'repeat(3, 200px)',
         boxShadow: '15px 10px 5px #0b0c0b',
     }
+
+    const imageStyle = {
+        backgroundImage: 'url(/images/monster.jpg)',
+        backgroundSize: 'cover',
+        width: '600px',
+        height: '600px',
+        boxShadow: '0 0 45px #d5d21c',
+    }
+
     return (
         <>
             <Button onClick={() => handleShuffle()} />
-            <div style={boardStyle}>
+            {!state.completed && <div style={boardStyle}>
                 {state.items.map((square, index) =>
                     <Square key={index} value={square} onClick={() => handleMove(index)} />
                 )}
-            </div>
+            </div>}
+            {state.completed && <div style={imageStyle}></div>}
         </>
     )
 }
